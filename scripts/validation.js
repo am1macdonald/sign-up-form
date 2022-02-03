be8ad0afed7a1c6686cb0aa86642fbb4dbd7799a
am1macdonald <am1macdonald@gmail.com>
2022-02-03 02:16:22 -0500

    styling and validation

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,9 +1,13 @@
 const validate = (() => {
   let password = document.getElementById('password');
   let confirm = document.getElementById('confirm-password');
+  let submit = document.getElementById('submit');
 
-  
-
-
-
-})
+  submit.addEventListener('submit', (e) => {
+    if (password.value !== confirm.value) {
+      e.preventDefault();
+      confirm.setCustomValidity("does not match");
+      alert('does not match!');
+    }
+  })
+})()
